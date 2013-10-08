7b473ff311d71dbe13e35d321c5ae21a86a605b1
eptwalabha <damiengabrielle@gmail.com>
2013-10-08 13:03:50 +0200

    Création de la page HTML pour afficher le graphique
    Modification de la classe PanelGraph:
    	- ajouts des axes (x et y) note: je dois changer ça pour que le graphe
    puisse accepter plus que deux axes.
    	- ajout de l'élément canvas qui est la zone de dessin
    	- ajout de la méthode 'drawGraph()' qui affiche le graphe en fonction
    de la taille du canvas.
    	
    Modification de la classe DotGraph:
    	- changement de type de couleur ( Array( r, g, b ) ).

--- src/class_dot.js
+++ src/class_dot.js
@@ -1,10 +1,10 @@
 function DotGraph() {
 	
 	this.values = new Array();
-	this.color = "AAA";
+	this.color = new Array( 0, 0, 0 );
 	
 	this.setValueForADimension = function( dim, value ) {
-		this.values[ dim ] = values;
+		this.values[ dim ] = value;
 	};
 	
 	this.getValueOfADimension = function( dim ) {
@@ -19,7 +19,15 @@ function DotGraph() {
 		return this.values.length;
 	};
 	
-	this.getDistance = function( dot ) {
-		return (dot.x-this.x)^2;
+	this.getSquareDistance = function( dot ) {
+		var sum = 0;
+		var dim1 = this.getNumberOfDimension();
+		var dim2 = dot.getNumberOfDimension();
+		var dim = ( dim1 < dim2 ) ? dim1 : dim2;
+		
+		for ( var i = 0; i < dim; i++ ) {
+			sum += Math.pow( ( dot.values[ i ] - this.values[ i ] ), 2 );
+		}
+		return sum;
 	};
 };
--- src/class_graph.js
+++ src/class_graph.js
@@ -1,6 +1,22 @@
-function PanelGraph() {
+function PanelGraph( x_axis, y_axis, canvas ) {
 	
 	this.dots = new Array();
+	this.canvas = canvas;
+	this.x_axis = x_axis;
+	this.y_axis = y_axis;
+	
+	this.setGraphAxes = function( x_axis, y_axis ) {
+		this.x_axis = x_axis;
+		this.y_axis = y_axis;
+	};
+	
+	this.getXAxis = function() {
+		return this.x_axis;
+	};
+	
+	this.getYAxis = function() {
+		return this.y_axis;
+	};
 	
 	this.addDot = function( dot ) {
 		this.dots.push( dot );
@@ -15,14 +31,16 @@ function PanelGraph() {
 	this.getColorFromDotPosition = function( position ) {
 		
 		if ( this.dots.length == 0 )
-			return "000";
+			return new Array( 0, 0, 0 );
 			
 		var min = -1;
 		var closest_dot = null;
 		
-		for( dot in this.dots ) {
+		for( var i = 0, size = this.dots.length; i < size; i++ ) {
 			
-			var dist = dot.getDistance( position );
+			var dot = this.dots[ i ];
+			
+			var dist = dot.getSquareDistance( position );
 			if ( min == -1 || dist < min ) {
 				min = dist;
 				closest_dot = dot;
@@ -45,13 +63,59 @@ function PanelGraph() {
 			var dot = new DotGraph();
 			dot.setValueForADimension( 0, Math.random()*50 );
 			dot.setValueForADimension( 1, Math.random()*50 );
-			var r =  Math.random() * 16;
-			var g =  Math.random() * 16;
-			var b =  Math.random() * 16;
+			var r =  Math.round( Math.random() * 255 );
+			var g =  Math.round( Math.random() * 255 );
+			var b =  Math.round( Math.random() * 255 );
 			
-			dot.color = "" + r.toString(16) + "" + g.toString(16) + "" + b.toString(16);
+			dot.color = new Array( r, g, b );
 			
 			this.dots.push( dot );
 		}
+	};
+	
+	this.setCanvas = function( canvas ) {
+		this.canvas = canvas;
+	};
+	
+	this.drawGraph = function() {
+		
+		var date = new Date();
+		var start = date.getMilliseconds();
+		
+		var xsize = this.canvas.width;
+		var ysize = this.canvas.height;
+		var context = this.canvas.getContext( "2d" );
+		var img = context.createImageData( 1, 1 );
+				
+		for ( var x = 0; x < xsize; x++ ) {
+			for ( var y = 0; y < ysize; y++ ) {
+				
+				var x_graph = ( x * this.x_axis ) / xsize;
+				var y_graph = ( y * this.y_axis ) / ysize;
+				
+				var pos = new DotGraph();
+				pos.setValueForADimension( 0, x_graph );
+				pos.setValueForADimension( 1, y_graph );
+				
+				var color = this.getColorFromDotPosition( pos );
+				
+				img.data[ 0 ] = color[ 0 ];
+				img.data[ 1 ] = color[ 1 ];
+				img.data[ 2 ] = color[ 2 ];
+				img.data[ 3 ] = 255;
+				
+				context.putImageData( img, x, ysize - y );
+			}
+		}
+		
+		for ( dot in this.dots ) {
+			this.drawDotCross( dot );
+		}
+		
+		return date.getMilliseconds() - start;
+	};
+	
+	this.drawDotCross = function( dot ) {
+		// TODO
 	};
 };
